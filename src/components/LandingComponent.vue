cc76682781a42fa0fd6fa34c69ca18002cfbffbc
Victor <victordevos.pro@gmail.com>
2024-10-15 09:00:23 +0200

    récupère userinscription et landing component

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="contain">
-    <div class="row">
-      <!-- Colonne pour les images à gauche -->
-      <!-- Colonne pour les images à gauche -->
-      <div class="col-lg-6 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center">
-        <div class="img-container">
-          <div v-for="product in lastImages" :key="product.id" class="image-wrapper">
-            <img :src="getImagePath(product.image)" :alt="product.titre" class="custom-img">
+  <div class="cont-global">
+        <div class="cont-image">
+          <div v-for="product in lastImages" :key="product.id" class="image-product">
+            <img :src="getImagePath(product.image)" :alt="product.titre" class="image-unique">
             <img />
           </div>
+          <div class="fondu"></div>
         </div>
-      </div>
 
-      <!-- Colonne pour le texte à droite -->
-      <div class="col-lg-6 col-md-6 col-sm-12 d-flex flex-column justify-content-start mt-5 text-center">
-        <div class="box-shadow">
+        <div class="cont-info">
           <h3>Inscrivez-vous dès maintenant et commencez à remplir vos stocks !</h3>
           <ButtonComponent text="Inscrivez-vous" color="#CA8465" />
           <p>Ou connectez-vous <u style="cursor: pointer">ici</u> et commencez vos achats immédiatement !</p>
         </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -54,91 +46,71 @@ export default {
 </script>
 
 <style scoped>
-.row {
-  height: 100%;
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.img-container {
-  margin-top: 50px;
-  margin-bottom: 50px;
-}
-
-.custom-img {
-  width: 170px;
-  /* Taille des images */
-  height: auto;
-  /* Conserve les proportions */
-}
-
-.image-wrapper {
-  width: 100%;
+.cont-global {
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 100px;
 }
 
-/* Appliquer progressivement plus de transparence */
-.image-wrapper:nth-child(1) img {
-  opacity: 1;
-  /* Premier élément totalement visible */
+.cont-image {
+  display: flex;
+  flex-direction: column;
 }
 
-.image-wrapper:nth-child(2) img {
-  opacity: 0.7;
-  /* Deuxième élément légèrement transparent */
+.image-unique {
+  height: 200px; width: 140px;
+  object-fit: cover;
+  object-position: center;
 }
 
-.image-wrapper:nth-child(3) img {
-  opacity: 0.3;
-  /* Troisième élément plus transparent */
-}
-
-.image-wrapper:nth-child(4) img {
-  opacity: 0.2;
-  /* Quatrième élément encore plus transparent */
-}
-
-
-
-
-/* Styles pour la colonne avec box-shadow */
-.box-shadow {
-  background-color: white;
-  /* Assure que l'arrière-plan soit blanc */
+.cont-info {
+  max-width: 35%; height: 100%;
   padding: 80px;
-  /* Ajoute de l'espace intérieur */
   border-radius: 15px;
-  /* Arrondit les coins */
   box-shadow: 0 16px 30px rgba(0, 0, 0, 0.3);
-  /* Box shadow */
   transition: box-shadow 0.3s ease;
-  /* Transition pour un effet lors du survol */
+  text-align: center;
+
   display: flex;
-  /* Ne prend que la largeur et la hauteur nécessaires */
   flex-direction: column;
   justify-content: center;
   gap: 30px;
-  margin-top: 150px;
-  margin-left: auto;
-  /* Centre horizontalement */
-  margin-right: auto;
-  /* Centre horizontalement */
-  width: 100%;
-  height: 400px;
 }
-
-.box-shadow h3 {
-  font-size: 1.5rem;
-}
-
-.box-shadow button {
-  width: fit-content;
-  margin: 0 auto 60px;
-}
-
 
 /* Effet de survol */
-.box-shadow:hover {
+.cont-info:hover {
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
-  /* Augmente l'ombre au survol */
 }
+
+@media only screen and (max-width: 950px) {
+  .cont-global{
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .cont-image{
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-bottom: 30px;
+    max-width: 600px;
+  }
+
+  .cont-info{
+    max-width: 550px;
+  }
+}
+
+@media only screen and (max-width: 550px) {
+  .image-unique{
+    width: 80px; height: 130px;
+  }
+}
+
 </style>
--- src/views/UserInscription.vue
+++ src/views/UserInscription.vue
@@ -1,33 +1,15 @@
 <template>
-    <div class="contain">
-        <SidePanel/>
-        <!-- <div class="contain-info">
-            <img :src="require('@/assets/icons/arrow_back.svg')" alt="icône arrière" class="icon" />
-            <div class="contain-title">
-                <svg class="logo" enable-background="new 0 0 24 24" height="60px" id="Layer_1" version="1.1"
-                    viewBox="0 0 24 24" width="60px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <g>
-                        <path
-                            d="M19.7,0.5H4.3c-2.1,0-3.8,1.7-3.8,3.8v15.4c0,2.1,1.7,3.8,3.8,3.8h15.4c2.1,0,3.8-1.7,3.8-3.8V4.3   C23.5,2.2,21.8,0.5,19.7,0.5z M19.1,16.9c0,0.3-0.2,0.5-0.5,0.5H8.4c-1.9,0-3.5-1.6-3.5-3.5V7.1c0-0.3,0.2-0.5,0.5-0.5h2.8   c0.3,0,0.5,0.2,0.5,0.5v6.1c0,0.1,0,0.1,0.1,0.1h1.5l0-6.2c0-0.3,0.2-0.5,0.5-0.5h2.6c0.3,0,0.5,0.2,0.5,0.5v6.2h1.6V7.1   c0-0.3,0.2-0.5,0.5-0.5h2.8c0.3,0,0.5,0.2,0.5,0.5V16.9z" />
-                    </g>
-                </svg>
-                <h1>Créer un compte WebWares</h1>
-                <p>Vous avez déjà un compte ? <router-link to="/user-connection" class="btn-navbar">Se
-                        connecter</router-link></p>
-            </div>
-            <div class="legal">
-                <p>WebWares.fr - Politique relative aux cookies et Charte de protection des données</p>
-                <p>© Inter WebWares Systems B.V. 1999-2024</p>
-            </div>
-        </div> -->
-        <div class="container-form">
-            <UserFormComponent />
-        </div>
-    </div>
-    <div class="container-form">
-      <UserFormComponent />
-    </div>
+  <div class="contain">
+      <SidePanel>
+          <h1>Créez un compte WebWares</h1>
+          <p>
+              Vous avez déjà un compte ?
+              <router-link to="/user-connection" class="btn-navbar">Se connecter</router-link>
+          </p>
+      </SidePanel>
+      <div class="contain-form">
+          <UserFormComponent />
+      </div>
   </div>
 </template>
 
@@ -35,10 +17,10 @@
 import UserFormComponent from '@/components/UserFormComponent.vue';
 import SidePanel from '@/components/SidePanel.vue';
 export default {
-    components: {
-        UserFormComponent,
-        SidePanel
-    }
+  components: {
+      UserFormComponent,
+      SidePanel
+  }
 }
 
 </script>
@@ -52,8 +34,7 @@ export default {
   padding: 0;
 }
 
-.container {
-  width: 100%;
+.contain {
   display: flex;
   align-items: center;
   gap: 20px;
@@ -69,36 +50,8 @@ a {
   color: #d9b596;
 }
 
-.container-info {
-  background-color: #592b02;
-  height: 100vh;
-  padding: 20px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.container-title {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.icon {
-  width: 40px;
-}
-
-.logo {
-  fill: #f1f1f1;
-  margin-bottom: 10px;
-}
-
-.legal {
-  font-size: 0.8rem;
-}
-
-.container-form {
+.contain-form {
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
