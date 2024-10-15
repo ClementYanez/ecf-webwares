4aa8c5a4015954dbf6d4f25b2cba43a6305b8a23
Victor <victordevos.pro@gmail.com>
2024-10-16 00:17:23 +0200

    récap commande et cb

--- src/views/OrderPayement.vue
+++ src/views/OrderPayement.vue
@@ -1,15 +1,60 @@
 <template>
-    <div>
-
+    <HeaderComponent />
+    <div class="global-container">
+        <div class="livraison-container">
+          <h2>Information de livraison</h2>
+          <div>
+            <p>Nom : {{ user.raisonSociale }}</p>
+            <p>N° SIRET : {{  user.siret }}</p>
+            <p>Email : {{ user.email }}</p>
+            <p>Adresse : {{ user.adresse }}, {{ user.codePostal }} {{ user.ville }}</p>
+          </div>
+        </div>
     </div>
-</template>
+    <PaymentComponent />
+    
+  </template>
+  
+  <script>
+  import HeaderComponent from '@/components/HeaderComponent.vue';
+  import PaymentComponent from '@/components/PaymentComponent.vue';
+  
+  
+  export default {
 
-<script>
-    export default {
-        
-    }
-</script>
+    components: {
+      HeaderComponent,
+      PaymentComponent,
+      
+    },
+    data() {
+      return {
+        user: {},
+      };
+    },
+    created() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user) {
+        this.user = user; // Récupérer les données de l'utilisateur connecté
+      }
+    },
+  };
+  </script>
+  
+  <style scoped>
+.global-container{
+    padding-top: 200px;
+}
 
-<style scoped>
-
-</style>
+.livraison-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* Ajusté pour aligner le contenu en haut */
+  align-items: center;
+  width: 40%;
+  padding: 40px;
+  margin: 0 auto;
+  box-shadow: 0 0 10px #d9b596;
+  }
+  </style>
+  
